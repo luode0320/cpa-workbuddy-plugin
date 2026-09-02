0b2b1e40bcbbe6b187038eba1a18bb576c15d3b3
luode <luode0320@qq.com>
2026-09-03 01:11:35 +0800

    chore(release): bump workbuddy-token-usage 0.2.1 -> 0.2.2 (SSE 实时通知)

--- token-usage-tracker/main.go
+++ token-usage-tracker/main.go
@@ -171,7 +171,7 @@ type registrationCapability struct {
 }
 
 // version is injected at build time via -ldflags "-X main.version=...".
-var version = "0.2.1"
+var version = "0.2.2"
 
 func trackerRegistration() registration {
 	return registration{
